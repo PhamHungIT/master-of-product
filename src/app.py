1bacb32ce9b68843abd9f8b3415031d74bae007b
Pham Van Hung <phamhung1010.it@gmail.com>
2023-02-09 16:46:23 +0700

    Synchronize data from many sources

---
+++ src/app.py
@@ -0,0 +1,52 @@
+import search_model as SearchModel
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index.html')
+def index():
+   return render_template('index.html')
+
+@app.route('/search.html', methods = ['POST', 'GET'])
+def search():
+   if request.method == 'POST':
+      query = request.form['query']
+      # Call the detectAttr to analyze the query
+      searchQuery = SearchModel.detectAttr(SearchModel.attrs, query)
+      # Get the list of matched attribute titles
+      fields = list(searchQuery.keys())
+      # Get the new query string that has filtered out the attribute titles
+      text = " ".join(list(searchQuery.values()))
+      # Call the search method to retrieve docs based on new query and matched fields
+      res = SearchModel.obj.search(text, searchQuery)
+
+      dct = {}
+      for doc in res['hits']['hits']:
+         dct[doc['_source']['name']] = doc['_source']
+
+      print(query)
+      print("Located attributes: " + " ".join(fields))
+      corresponding_attributes = ", ".join(fields)
+      print(dct)
+      no_results = len(dct)
+      return render_template('search.html', query = query, results = dct, no_results = no_results,
+            corresponding_attributes=corresponding_attributes)
+
+   if request.method == 'GET':
+      return render_template('search.html')
+
+@app.route('/about.html')
+def about():
+   return render_template('about.html')
+
+@app.route('/data.html')
+def data():
+   return render_template('data.html')
+
+@app.route('/references.html')
+def references():
+   return render_template('references.html')
+
+if __name__ == '__main__':
+   app.run(debug=True)
---
+++ src/laptopscraper/dmx.py
@@ -0,0 +1,145 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import csv
+from selenium.webdriver.common.by import By
+
+# Options
+chrome_options = Options()
+chrome_options.add_argument("--incognito")
+chrome_options.add_argument("--headless")
+
+import re
+#text_after = re.sub(regex_search_term, regex_replacement, text_before)
+def convert(value):
+    value = re.sub(r'\D', "",value)
+    return int(value)
+
+product_links = []
+url = 'https://www.dienmayxanh.com/laptop#c=44&o=9&pi=10'
+
+browser = webdriver.Chrome(executable_path="chromedriver.exe", chrome_options=chrome_options)
+browser.implicitly_wait(3) # seconds
+browser.get(url)
+
+links = browser.find_elements(By.XPATH, '//*[@class="main-contain"]')  # Vị trí chứa đường dẫn sản phẩm
+for i in links:
+    link = i.get_attribute("href")   # Lấy đường dẫn và thêm vào list
+    product_links.append(link)
+
+csv_columns = ['Product','Price','Brand','Core','RAM','ScrSize','GraphicCard','Drive_Type','Capacity','OperSystem','Weight','Madein', 'Since','Shop','URL']
+with open('data/raw/dmx.csv', "a", encoding="utf8") as f:
+    writer = csv.DictWriter(f, fieldnames=csv_columns)
+    writer.writeheader()
+
+for link in product_links:
+    try:
+        browser.get(link)
+        product_id = browser.find_element(By.XPATH, '/html/body/section[1]').get_attribute("data-id")
+        product = browser.find_element(By.XPATH, '/html/body/section[1]/h1').text
+        brand = browser.find_element(By.XPATH, '/html/body/section[1]/ul/li[2]/a').text
+        brand = re.sub(r'Laptop ','',brand)
+        price = ''
+        try:
+            price = browser.find_element(By.XPATH, '/html/body/section[1]/div[3]/div[2]/div[1]/div[2]/div/p[1]').text
+            price = re.sub(r'\D', "",price)
+        except:
+            pass
+        
+        try:
+            price = browser.find_element(By.XPATH, '/html/body/section[1]/div[3]/div[2]/div[1]/div[1]/div/strong').text
+            price = re.sub(r'\D', "",price)
+        except:
+            pass
+
+        try:
+            price = browser.find_element(By.XPATH, '/html/body/section[1]/div[3]/div[2]/div[2]/div[2]/div/p').text
+            price = re.sub(r'\D', "",price)
+        except:
+            pass
+
+        try:
+            price = browser.find_element(By.XPATH, '/html/body/section[1]/div[3]/div[2]/div[3]/div[2]/div/p').text
+            price = re.sub(r'\D', "",price)
+        except:
+            pass
+
+        shop = 'Dienmayxanh'
+        url = 'https://www.dienmayxanh.com/Product/GetGalleryItemInPopup?productId=' + product_id +'&isAppliance=false&galleryType=5'
+        browser.get(url)
+        ram = browser.find_element(By.XPATH, '/html/body/div/div/div[2]/ul/li[1]/div[2]/a').text
+        ram = convert(ram)
+        scr = browser.find_element(By.XPATH, '/html/body/div/div/div[3]/ul/li[1]/div[2]/span').text
+        scr = re.sub(r' \D*', '', scr)
+        try:
+            capacity = browser.find_element(By.XPATH, '/html/body/div/div/div[2]/ul/li[5]/div[2]/a').text
+        except:
+            pass
+        try:
+            capacity = browser.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/ul/li[5]/div[2]/a').text
+        except:
+            pass
+
+        try:
+            capacity = browser.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/ul/li[5]/div[2]/p[1]/a').text
+        except:
+            pass
+
+        try:
+            capacity = browser.find_element(By.XPATH, '/html/body/div/div/div[2]/ul/li[5]/div[2]/span[1]').text
+        except:
+            pass
+
+        drive_type = 'SSD'
+        try:
+            core = browser.find_element(By.XPATH, '/html/body/div/div/div[1]/ul/li[1]/div[2]/span/a').text
+        except:
+            pass
+
+        try:
+            core = browser.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/ul/li[1]/div[2]/span/a[1]').text
+        except:
+            pass
+
+        weight = re.sub(r'(?:Dài.* - Rộng.*Nặng )',"",browser.find_element(By.XPATH, '/html/body/div/div/div[6]/ul/li[1]/div[2]/span').text)
+        weight = re.sub(r'(?: kg)','',weight)
+        since = browser.find_element(By.XPATH, '/html/body/div/div/div[7]/ul/li[3]/div[2]/span').text
+        graphic_card = browser.find_element(By.XPATH, '/html/body/div/div/div[4]/ul/li[1]/div[2]/span').text
+        try:
+            opsys = browser.find_element(By.XPATH, '/html/body/div[2]/div/div[7]/ul/li[2]/div[2]/a').text
+        except:
+            pass
+
+        try:
+            opsys = browser.find_element(By.XPATH, '/html/body/div/div/div[7]/ul/li[2]/div[2]/a').text
+        except:
+            pass
+
+        try:
+            opsys = browser.find_element(By.XPATH, '/html/body/div/div/div[7]/ul/li[2]/div[2]/span').text
+        except:
+            pass
+
+        data = {
+                "Product" : product,
+                "Price" : price,
+                "Brand" : brand,
+                "Core" : core,
+                "RAM" : ram,
+                "ScrSize" : scr,
+                "GraphicCard" : graphic_card,
+                "Drive_Type" : drive_type,
+                "Capacity" : capacity,
+                "OperSystem" : opsys,
+                "Weight" : weight,
+                "Madein" : "",
+                "Since" : since,
+                "Shop": 'Dienmayxanh',
+                "URL":link,
+                } 
+        with open('data/raw/dmx.csv', "a", encoding="utf8") as f:
+            writer = csv.DictWriter(f, fieldnames=csv_columns)
+            writer.writerow(data)
+    except:
+        pass
+
+browser.close()
