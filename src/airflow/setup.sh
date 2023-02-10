2794ae8f1b04b31a49ebcb9170bde91202777cf6
Pham Van Hung <phamhung1010.it@gmail.com>
2023-02-10 08:01:23 +0700

    Update client

---
+++ src/airflow/setup.sh
@@ -0,0 +1,10 @@
+# Install dependencies
+sudo apt-get install software-properties-common
+sudo apt-add-repository universe
+sudo apt-get update
+sudo apt-get install python-setuptools
+sudo apt install python3-pip
+
+sudo apt-get install libmysqlclient-dev
+sudo apt-get install libssl-dev
+sudo apt-get install libkrb5-dev
